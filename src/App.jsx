6585d86d15192bc691f4378ed61938bfe345b1fd
masanori iha <masanori.iha@gmail.com>
2022-02-15 19:23:48 -0300

    excluindo contador e colocando um pouco de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/repositoryList';
 import './styles/global.scss';
 
 export function App(){
     return  (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 }
